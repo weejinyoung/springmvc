1a32e668984503155ec1b6c11153782f613ba127
Jn W <a8118199@gmail.com>
2022-04-02 08:08:46 +0900

    yesterday study

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+
+    @GetMapping
+    public String user() {
+        return "get users";
+    }
+
+    @PostMapping
+    public String addUser() {
+        return "post users";
+    }
+
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable String userId) {
+        return "get userId = " + userId;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+        return "delete userId = " + userId;
+    }
+}
